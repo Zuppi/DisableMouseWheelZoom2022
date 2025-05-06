8ba1c9b06edb97bc7b136b7889a905fcec84efc3
Zuppi <Zuppi@ZUPPIMASIN>
2025-05-06 20:11:35 +0300

    Add project files.

---
+++ TextViewScrollDisabler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Windows;
+using System.Windows.Media;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace DisableMouseWheelZoom2022
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    internal class TextViewScrollDisabler : IWpfTextViewCreationListener
+    {
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            textView.Options.SetOptionValue(DefaultWpfViewOptions.EnableMouseWheelZoomId, false);
+        }
+    }
+}
